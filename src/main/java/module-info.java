8ca7fc9c877608aa0f48c91f45006a61e05d666e
Viktor Vaňo <viktorvano@gmail.com>
2019-09-28 18:39:47 +0200

    Upload of the project
    first upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.yourname {
+    requires javafx.controls;
+    requires jzy3d.api;
+    requires jogl.all;
+    requires java.desktop;
+    requires javafx.swing;
+    requires log4j;
+    exports com.yourname;
+}
